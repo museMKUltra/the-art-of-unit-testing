5003a6a1ebf494030c8fd07322333e1e23fdb3a5
Roller Lo <jasonbh02@gmail.com>
2021-02-04 22:15:16 +0800

    lower & upper case for file extension

--- UnitTestProject/LogAn/LogAnalyzer.cs
+++ UnitTestProject/LogAn/LogAnalyzer.cs
@@ -1,10 +1,12 @@
+using System;
+
 namespace UnitTestProject.LogAn
 {
     public class LogAnalyzer
     {
         public bool IsValidLogFileName(string fileName)
         {
-            if (fileName.EndsWith(".SLF"))
+            if (!fileName.EndsWith(".SLF", StringComparison.CurrentCultureIgnoreCase))
             {
                 return false;
             }
--- UnitTestProject/LogAn/LogAnalyzerTests.cs
+++ UnitTestProject/LogAn/LogAnalyzerTests.cs
@@ -8,7 +8,34 @@ namespace UnitTestProject.LogAn
         [Test]
         public void IsValidLogFileName_BadExtension_ReturnFalse()
         {
-            
+            // arrange
+            var analyzer = new LogAnalyzer();
+
+            // act
+            var result = analyzer.IsValidLogFileName("filewithbadextension.foo");
+
+            // assert
+            Assert.False(result);
+        }
+
+        [Test]
+        public void IsValidLogFileName_GoodExtensionLowercase_ReturnTrue()
+        {
+            var analyzer = new LogAnalyzer();
+
+            var result = analyzer.IsValidLogFileName("filewithbadextension.slf");
+
+            Assert.True(result);
+        }
+
+        [Test]
+        public void IsValidLogFileName_GoodExtensionUppercase_ReturnTrue()
+        {
+            var analyzer = new LogAnalyzer();
+
+            var result = analyzer.IsValidLogFileName("filewithbadextension.SLF");
+
+            Assert.True(result);
         }
     }
 }
