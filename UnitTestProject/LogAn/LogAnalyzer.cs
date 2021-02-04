117c3298fc52d6018b4200a785efe4bb76e79bed
Roller Lo <jasonbh02@gmail.com>
2021-02-04 12:43:26 +0800

    build log analyzer class & test method

---
+++ UnitTestProject/LogAn/LogAnalyzer.cs
@@ -0,0 +1,15 @@
+namespace UnitTestProject.LogAn
+{
+    public class LogAnalyzer
+    {
+        public bool IsValidLogFileName(string fileName)
+        {
+            if (fileName.EndsWith(".SLF"))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ UnitTestProject/LogAn/LogAnalyzerTests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace UnitTestProject.LogAn
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        [Test]
+        public void IsValidLogFileName_BadExtension_ReturnFalse()
+        {
+            
+        }
+    }
+}
